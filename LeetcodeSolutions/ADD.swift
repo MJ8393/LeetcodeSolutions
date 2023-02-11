70879282a7c79e15a48326c6b3d0e096989d929d
Khabibullokh18022004 <93874103+Khabibullokh18022004@users.noreply.github.com>
2023-02-11 23:53:11 +0500

    Habib's problem

---
+++ LeetcodeSolutions/ADD.swift
@@ -0,0 +1,11 @@
+class Solution
+{
+    func plusOne(_ digits: [Int]) -> [Int] {
+        guard let lastDigit = digits.last else { return [] }
+        
+        if lastDigit == 9 {
+            return digits.count == 1 ? [1, 0] : plusOne(digits.dropLast()) + [0]
+        }
+        return digits.dropLast() + [lastDigit + 1]
+    }
+}
