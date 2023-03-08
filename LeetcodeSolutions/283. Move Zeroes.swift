15684ae4c75e088cc59a945ed56f6a6707d15687
Mekhriddin Jumaev <jumaevmekhriddin10@gmail.com>
2023-03-09 02:21:22 +0500

    Solution 283

---
+++ LeetcodeSolutions/283. Move Zeroes.swift
@@ -0,0 +1,27 @@
+//
+//  283. Move Zeroes.swift
+//  LeetcodeSolutions
+//
+//  Created by Mekhriddin Jumaev on 09/03/23.
+//
+
+import Foundation
+
+class Solution283 {
+    func moveZeroes(_ nums: inout [Int]) {
+        var l = 0
+        var r = 0
+        while(r != nums.count) {
+            if nums[r] != 0 {
+                var x = nums[l]
+                nums[l] = nums[r]
+                nums[r] = x
+                l += 1
+            }
+            r += 1
+        }
+    }
+}
+
+// Time: O(n)
+// Space: O(1)
