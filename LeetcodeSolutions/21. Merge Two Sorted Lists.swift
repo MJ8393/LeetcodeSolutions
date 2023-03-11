33dc5424c5ed25c3a6ff32a49e54a784c6718de4
Mekhriddin Jumaev <jumaevmekhriddin10@gmail.com>
2023-03-11 22:14:33 +0500

    Solution 21

---
+++ LeetcodeSolutions/21. Merge Two Sorted Lists.swift
@@ -0,0 +1,44 @@
+//
+//  File.swift
+//  LeetcodeSolutions
+//
+//  Created by Mekhriddin Jumaev on 11/03/23.
+//
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+
+import Foundation
+
+class Solution21 {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        var list1 = l1
+        var list2 = l2
+
+        let head: ListNode = ListNode(0)
+        var tmp = head
+        while list1 != nil && list2 != nil {
+            let v1 = list1!.val
+            let v2 = list2!.val
+
+            if v1 > v2 {
+                tmp.next = list2!
+                list2 = list2?.next
+            } else {
+                tmp.next = list1!
+                list1 = list1?.next
+            }
+            tmp = tmp.next!
+        }
+        tmp.next = list1 ?? list2
+        return head.next
+    }
+}
